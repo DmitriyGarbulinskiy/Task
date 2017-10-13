dcffc1d12a53100834ff371b58178579cba07749
DmitriyGarbulinskiy <DmitriyGarbulinskiy@users.noreply.github.com>
2017-10-14 00:57:49 +0300

    Update Market.java

--- src/main/java/work/Market.java
+++ src/main/java/work/Market.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 // The program emulates 30 days of work of the store
 // DB will be loaded from "test.csv" file. You will find information about products left after work in this file
 // Report about program work you can find in "report.txt"
-
+//
 // All information you can find in following classes :
 // "Stock" - This class "stores" DB and contains all functions to work with it
 // "CashOperation" - This class contains all cash operations
